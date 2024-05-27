9e3958022d478e2d59d8d62fe047a6f388c3e5ef
calculadora777 <calculadora522@gmail.com>
2024-05-27 14:12:49 -0300

    Add files via upload

---
+++ index.js
@@ -0,0 +1,36 @@
+var p_res = document.getElementById('p_res')
+
+function adiciona_res(item) {
+    p_res.innerHTML += item
+}
+function limpa() {
+    p_res.innerHTML = ''
+}
+function calcular() {
+
+    if (p_res.textContent[0] != '√' && p_res.textContent[0] != 'x') {
+        let resultado = eval(p_res.textContent)
+        p_res.innerHTML = resultado
+    } else if (p_res.textContent[0] == '√') {
+        let resultado = eval(p_res.textContent.slice(1))
+        resultado = resultado ** 0.5
+        p_res.innerHTML = resultado
+    } else if (p_res.innerHTML[0] == 'x') {
+        let resultado = eval(p_res.textContent.slice(1))
+        resultado = resultado * resultado
+        p_res.innerHTML = resultado
+    }
+    return
+}
+function adiciona_raiz() {
+    if (p_res.textContent == '') {
+        p_res.innerHTML += '√'
+    }
+    return
+}
+function adiciona_pot() {
+    if (p_res.textContent == '') {
+        p_res.innerHTML += 'x'
+    }
+    return
+}
